3db2da046eae87e060540ba14958c4e8166cdb2e
Matheus F. Mota <mxtheusmota@gmail.com>
2022-08-25 21:42:44 -0300

    Primeiro commit.

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home (){
+    return (
+        <>
+            <h1 className='titulo'>Teste</h1>
+            <img src="https://i.imgur.com/L6JKiS7.gif" alt="Imagem tela inicial" className='img'/>
+        </>
+    );
+}
+
+export default Home;
