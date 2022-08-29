d8d4cd887cb715926483367b52c2bf1d7281ecc8
Matheus F. Mota <mxtheusmota@gmail.com>
2022-08-29 00:48:29 -0300

    Adicionando navbar, footer e home.

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import {Typography, Box, Grid, Button} from '@mui/material';
 import './Home.css';
 
-function Home (){
+function Home() {
     return (
         <>
-            <h1 className='titulo'>Teste</h1>
-            <img src="https://i.imgur.com/L6JKiS7.gif" alt="Imagem tela inicial" className='img'/>
+            <Grid container direction="row" justifyContent="center" alignItems="center" style={{ backgroundColor: "#3F51B5" }}>
+                <Grid alignItems="center" item xs={6}>
+                    <Box paddingX={20} >
+                        <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" style={{ color: "white", fontWeight: "bold" }}>Seja bem vinde!</Typography>
+                        <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" style={{ color: "white", fontWeight: "bold" }}>Expresse aqui os seus pensamentos e opiniões!</Typography>
+                    </Box>
+                    <Box display="flex" justifyContent="center">
+                        <Box marginRight={1}>
+                        </Box>
+                        <Button variant="outlined" style={{ borderColor: "white", backgroundColor: "#3F51B5", color: "white" }}>Ver Postagens</Button>
+                    </Box>
+                </Grid>
+                <Grid item xs={6} >
+                    <img src="https://omatheusmota.github.io/fundamentos-web/assets/statics/me1.png" alt="" width="383px" height="480px" />
+                </Grid>
+                <Grid xs={12} style={{ backgroundColor: "white" }}>
+                </Grid>
+            </Grid>
         </>
     );
 }
